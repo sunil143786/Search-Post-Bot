0a43fe753c25d33d51a3f6fca2f8d5b504cde415
sunil143786 <141899550+sunil143786@users.noreply.github.com>
2024-03-03 08:20:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ API_ID       = 12124605
 API_HASH     = "5cf3577d85fd02286535ec2296934287"
 BOT_TOKEN    = "6921056041:AAFBNkm83lCqYmOVu17e45MwtfIWAZsuiSA"
 SESSION      = "BQFmTcUAa-CCCiB_J212WYl2_b3aNLdpDtx-KoBvh49K1JXndHfw7mcU2vuAoj3s52GUEp1GWdK2kjGePmGRhtI3TCjZctLi3SFAx9RBtX8aB9YC6ZnaIJQQl8TSeCv7EcOqfCbz5ySuq4OJ05CTSUaA5H9gJXKSg65flVDQw-Vzvvdvsnmx82NPlgkPYopb6qgb5-R240v41W360uxYziT-ePa33Qzwx_7wY02dqeQ1i82y-o1Qe4wbp5U3-iQ5qQx6ZsSdm1jLtt6ZKnBpCzO0n2sjYVNukeqAEuLUcZ_QI0Gm-_j4UEHkKq_f-eP3fAYdB87avKCB360wdPVrMxvBBL7ecQAAAAFPj9HTAA"
-DATABASE_URI = "mongodb+srv://Sahilbot:Sahilbot@cluster0.jeax7jn.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://sunil143:sunil143@cluster0.ok6lxfd.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001539693613
 ADMIN        = 5272400279
